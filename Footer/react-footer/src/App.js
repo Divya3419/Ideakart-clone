2ab92d906d9e49cd84a8d9515da45c3651651ae6
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-05-04 13:22:53 +0530

    Footer

---
+++ Footer/react-footer/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { Footer } from './Components/Footer'
+//import {Search} from './Components/Search'
+//import {PrivacyPolicy} from './Components/PrivacyPolicy'
+//import {RefundPolicy} from './Components/RefundPolicy'
+//import {EarnMoneyOnline} from './Components/MoneyOnline'
+
+function App() {
+  return (
+    <div className="App">
+     <Footer />
+     {/* <Search /> */}
+     {/* <PrivacyPolicy /> */}
+     {/* <RefundPolicy /> */}
+     {/* <EarnMoneyOnline /> */}
+    </div>
+  );
+}
+
+export default App;
